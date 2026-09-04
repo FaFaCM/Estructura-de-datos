8e05ef5b63961b98e6a62df5c8331771bdc86abf
FaFaCM <rafaelcmtzcen151@gmail.com>
2026-09-03 21:15:43 -0700

    Add files via upload

---
+++ C#/Busqueda_binaria/Busqueda_binaria/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static int[] arreglo = { 3, 8, 15, 23, 42, 56, 70 };
+
+    static int BusquedaBinaria(int valor)
+    {
+        int izquierda = 0;
+        int derecha = arreglo.Length - 1;
+
+        while (izquierda <= derecha)
+        {
+            int medio = (izquierda + derecha) / 2;
+
+            if (arreglo[medio] == valor)
+            {
+                return medio;
+            }
+            else if (arreglo[medio] < valor)
+            {
+                izquierda = medio + 1;
+            }
+            else
+            {
+                derecha = medio - 1;
+            }
+        }
+        return -1;
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine(BusquedaBinaria(23));
+    }
+}
---
+++ C#/Busqueda_secuencial/Busqueda_secuencial/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static int[] arreglo = { 15, 3, 42, 8, 23 };
+
+    static int BusquedaSecuencial(int valor)
+    {
+        for (int i = 0; i < arreglo.Length; i++)
+        {
+            if (arreglo[i] == valor)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine(BusquedaSecuencial(42));
+    }
+}
