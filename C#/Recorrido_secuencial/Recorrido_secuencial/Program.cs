5b6c52a9db8c3abb77164c33b7c7b3981c921b33
FaFaCM <rafaelcmtzcen151@gmail.com>
2026-09-03 21:12:51 -0700

    Add files via upload

---
+++ C#/Recorrido_secuencial/Recorrido_secuencial/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] arreglo = { 10, 20, 30, 40, 50 };
+
+        for (int i = 0; i < arreglo.Length; i++)
+        {
+            Console.WriteLine($"Posición {i}: {arreglo[i]}");
+        }
+    }
+}
