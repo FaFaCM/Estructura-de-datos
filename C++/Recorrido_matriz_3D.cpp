a80ed74d438de137408c90342b9ef6617345c3d0
FaFaCM <rafaelcmtzcen151@gmail.com>
2026-09-17 21:13:17 -0700

    Add files via upload

---
+++ C++/Recorrido_matriz_3D.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int matriz3D[2][2][2] = {
+        {{1, 2}, {3, 4}},
+        {{5, 6}, {7, 8}}
+    };
+
+    for (int k = 0; k < 2; k++)
+        for (int i = 0; i < 2; i++)
+            for (int j = 0; j < 2; j++)
+                cout << matriz3D[k][i][j] << " ";
+    cout << endl;
+
+    return 0;
+}
